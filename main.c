af085144f545e6c3d9ce90d63275976e60215d18
Bartosz Piekarski <296780@ameryk.fizyka.umk.pl>
2019-05-23 21:55:37 +0200

    "Drobny błąd" Poprawiłem literówkę w funkcji i dodałem losowanie oraz wyświetlanie początkowych położeń obiektów.

--- main.c
+++ main.c
@@ -11,19 +11,42 @@ typedef struct obiekt{
 	struct obiekt *next;
 }lista;
 
-lista *losujObjekty(lista **head,int ilosc){
+void losujObiekty(lista **head,int ilosc, int rozmiar, char tab[][rozmiar]){
 	srand(time(NULL));
-	int i;
-	*head=malloc(sizeof(lista));
+	int i,j,x,y;
+	for(i=0;i<rozmiar;i++){
+        for(j=0;j<rozmiar;j++){
+            tab[i][j]='#';
+        }
+	}
 	lista *current=*head;
-	for(i=1;i<ilosc;i++){
+	for(i=0;i<ilosc;i++){
 		current->sila=rand()%25;
 		current->zycia=rand()%3+1;
 		current->jedzenie=2;
+		x=rand()%rozmiar;
+		y=rand()%rozmiar;
+		while(tab[x][y]!='#'){
+            x=rand()%rozmiar;
+            y=rand()%rozmiar;
+		}
+		tab[x][y]='O';
+        current->x=x;
+        current->y=y;
 		current->next=malloc(sizeof(lista));
 		current=current->next;
 	}
-	return *head;
+	current->next=NULL;
+}
+
+void wypisz(int rozmiar, char tab[][rozmiar]){
+    int i,j;
+    for(i=0;i<rozmiar;i++){
+        for(j=0;j<rozmiar;j++){
+            printf("%c ",tab[i][j]);
+        }
+        puts("");
+    }
 }
 
 int main(){
@@ -37,7 +60,7 @@ int main(){
 		if(ilosc>rozmiar*rozmiar) printf("Ilosc obiektow nie moze przekraczac ilosci pol planszy!\n");
 	}while(ilosc>rozmiar*rozmiar);
 	lista *obiekty=malloc(sizeof(lista));
-	losujObiekty(&obiekty,ilosc);
-	int i,j;
+	losujObiekty(&obiekty,ilosc,rozmiar,plansza);
+	wypisz(rozmiar,plansza);
 	return 0;
 }
