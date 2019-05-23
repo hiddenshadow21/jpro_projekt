84b51b831fbdacc112af626e2f0b9645fd97b2a2
Bartosz Piekarski <296780@ameryk.fizyka.umk.pl>
2019-05-23 13:37:20 +0200

    Drugi commit

--- main.c
+++ main.c
@@ -11,12 +11,11 @@ typedef struct obiekt{
 	struct obiekt *next;
 }lista;
 
-void nowyObjekt(int ilosc){
+lista *losujObjekty(lista **head,int ilosc){
 	srand(time(NULL));
 	int i;
-	lista *obiekty=malloc(sizeof(lista));
-	obiekty->next=NULL;
-	lista *current=obiekty;
+	*head=malloc(sizeof(lista));
+	lista *current=*head;
 	for(i=1;i<ilosc;i++){
 		current->sila=rand()%25;
 		current->zycia=rand()%3+1;
@@ -24,6 +23,7 @@ void nowyObjekt(int ilosc){
 		current->next=malloc(sizeof(lista));
 		current=current->next;
 	}
+	return *head;
 }
 
 int main(){
@@ -36,14 +36,8 @@ int main(){
 		scanf("%d",&ilosc);
 		if(ilosc>rozmiar*rozmiar) printf("Ilosc obiektow nie moze przekraczac ilosci pol planszy!\n");
 	}while(ilosc>rozmiar*rozmiar);
-	nowyObjekt(ilosc);
+	lista *obiekty=malloc(sizeof(lista));
+	losujObiekty(&obiekty,ilosc);
 	int i,j;
-	for(i=0;i<rozmiar;i++){
-		for(j=0;j<rozmiar;j++){
-			plansza[i][j]=97+i+j;
-			printf("%c ",plansza[i][j]);
-		}
-		puts("");
-	}
 	return 0;
 }
