78c53480b37b5b41bdf81b62f9a9b927112c4661
Bartosz Piekarski <296780@ameryk.fizyka.umk.pl>
2019-05-26 17:20:17 +0200

    Nowe funkcje Dodałem symulację walki(usuwanie obiektów, które znajdują się na tym samym polu) i powstawania nowych obiektów, poprawiłem funkcję losującą początkowe obiekty(losowało o jeden obiekt za dużo).

--- main.c
+++ main.c
@@ -15,6 +15,9 @@ void losujObiekty();
 void wypisz();
 void ruch();
 void symulujDzien();
+void czyTaSamaPozycja();
+void usun();
+void dodajOb();
 
 int main(){
     srand(time(NULL));
@@ -30,7 +33,9 @@ int main(){
 
 	lista *obiekty=malloc(sizeof(lista));
 	losujObiekty(&obiekty,iloscOb,rozmiar,plansza);
-	symulujDzien(obiekty,rozmiar,plansza);
+	wypisz(rozmiar,plansza,obiekty); //plansza startowa
+    puts("");
+	symulujDzien(&obiekty,rozmiar,plansza);
 
 	return 0;
 }
@@ -43,8 +48,23 @@ void losujObiekty(lista **head,int iloscOb, int rozmiar, char tab[][rozmiar]){
         }
 	}
 	lista *current=*head;
+	current->sila=rand()%4;
+    current->zycia=rand()%3+1;
+    current->jedzenie=2;
+    x=rand()%rozmiar;
+    y=rand()%rozmiar;
+    while(tab[y][x]!='#'){
+        x=rand()%rozmiar;
+        y=rand()%rozmiar;
+    }
+    tab[y][x]='O';
+    current->x=x;
+    current->y=y;
+    current->next=NULL;
 	for(i=0;i<iloscOb;i++){
-		current->sila=rand()%25;
+        current->next=malloc(sizeof(lista));
+        current=current->next;
+		current->sila=rand()%4;
 		current->zycia=rand()%3+1;
 		current->jedzenie=2;
 		x=rand()%rozmiar;
@@ -56,10 +76,8 @@ void losujObiekty(lista **head,int iloscOb, int rozmiar, char tab[][rozmiar]){
 		tab[y][x]='O';
         current->x=x;
         current->y=y;
-        current->next=malloc(sizeof(lista));
-        current=current->next;
+        current->next=NULL;
 	}
-	current->next=NULL;
 }
 
 void wypisz(int rozmiar, char tab[][rozmiar], lista *head){
@@ -71,9 +89,11 @@ void wypisz(int rozmiar, char tab[][rozmiar], lista *head){
         }
     }
     while(current->next!=NULL){
+        printf("%d %d\n",current->x, current->y);
         tab[current->y][current->x]='O';
         current=current->next;
     }
+    puts("");
     for(i=0;i<rozmiar;i++){
         for(j=0;j<rozmiar;j++){
             printf("%c ",tab[i][j]);
@@ -113,18 +133,71 @@ void ruch(lista *current,int rozmiar){
     }
 }
 
-void symulujDzien(lista *head, int rozmiar, char tab[][rozmiar]){
+void symulujDzien(lista **head, int rozmiar, char tab[][rozmiar]){
     int i;
-    lista *current=head;
-    for(i=0;i<12;i++){
+    lista *current=*head;
+    for(i=0;i<4;i++){
         while(current->next!=NULL){
             ruch(current, rozmiar);
             current=current->next;
         }
-        current=head;
-        wypisz(rozmiar,tab,head);
+
+        current=*head;
+        wypisz(rozmiar,tab,*head);
+        czyTaSamaPozycja(head);
         puts("");
     }
+}
+
+void czyTaSamaPozycja(lista **head){
+    lista *obj1=*head, *obj2;
+    while(obj1->next!=NULL){
+            obj2=obj1->next;
+            while(obj2->next!=NULL){
+                if(obj1->x == obj2->x && obj1->y == obj2->y){
+                    if(obj1->sila > obj2->sila){
+                        usun(head,obj2);
+                    }
+                    else if(obj1->sila < obj2->sila){
+                        usun(head,obj1);
+                    }
+                    else{
+                        dodajOb(*head);
+                    }
+                }
+                obj2=obj2->next;
+            }
+            obj1=obj1->next;
+    }
+}
+
+void usun(lista **head, lista *doUsuniecia){
+    lista *current=*head;
+    if(*head==doUsuniecia){
+        current=current->next;
+        free(*head);
+        *head=current;
+    }
+    else{
+        while(current->next!=doUsuniecia){
+            current=current->next;
+        }
+        current->next=doUsuniecia->next;
+        free(doUsuniecia);
+    }
+}
+
+void dodajOb(lista *head){
+    lista *current=head;
+    while(current->next!=NULL){
+        current=current->next;
+    }
+    current->next=malloc(sizeof(lista));
+    current->next->zycia=rand()%3+1;
+    current->next->x=0;
+    current->next->y=0;
+    current->next->sila=rand()%4;
+    current->next->next=NULL;
 }
 
 
